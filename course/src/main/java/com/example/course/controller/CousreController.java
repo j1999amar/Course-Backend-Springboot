a34ba040b1cce2bcbc5a08cf57496ea985236b49
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-12-06 11:08:40 +0530

    View All Course

--- course/src/main/java/com/example/course/controller/CousreController.java
+++ course/src/main/java/com/example/course/controller/CousreController.java
@@ -1,5 +1,6 @@
 package com.example.course.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
@@ -8,6 +9,10 @@ public class CousreController {
     @PostMapping(path = "/addCourse",produces = "application/json",consumes = "application/json")
     public String AddCourse(){
         return "Add Course";
+    }
+    @GetMapping("/viewAllCourse")
+    public String ViewAllCourse(){
+        return "View All Course";
     }
 
 
